ef2b997d1d910ddbc3b61f41e1658a943ec82c1f
Tactical98 <preciousnwaigbo98@gmail.com>
2022-03-18 13:31:23 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,6 @@
 void print_remaining_days(int month, int day, int year)
 
 {
-
 	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month > 2 && day >= 60)
@@ -24,7 +23,7 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else 
+	else
 	{
 		if (month == 2 && day == 60)
 		{
