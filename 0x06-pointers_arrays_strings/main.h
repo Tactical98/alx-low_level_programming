830346de952ea8ecfb4b0861092694f4a98ad629
Tactical98 <preciousnwaigbo98@gmail.com>
2022-03-25 06:15:41 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int_strcmp(char *51, char *52);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char);
+
