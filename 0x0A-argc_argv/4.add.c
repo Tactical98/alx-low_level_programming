8c5a83cb68355b364560a8010e9a28b4e0ff52ed
Tactical98 <99683218+Tactical98@users.noreply.github.com>
2022-04-02 00:26:44 +0100

    Update 4.add.c

--- 0x0A-argc_argv/4.add.c
+++ 0x0A-argc_argv/4.add.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
- * main - adds positive numbers.
+ * main - multiplies two numbers.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
-
-	for (i = 1; i < argc; i++)
+	if (argc != 3)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		add += atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", add);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
